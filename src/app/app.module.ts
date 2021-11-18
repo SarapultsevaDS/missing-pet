9b3bb80c78f4af3a82d74c5dbb969b60c28821b7
Darya Vostroknutova <justdsarap@gmail.com>
2021-11-18 22:33:55 +0500

    feat: set search bar parameters

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { ToolbarComponent } from './toolbar/toolbar.component';
 import { MatCardModule } from '@angular/material/card';
+import { MatSelectModule } from '@angular/material/select';
 
 @NgModule({
   declarations: [
@@ -26,7 +27,8 @@ import { MatCardModule } from '@angular/material/card';
     MatToolbarModule,
     MatIconModule,
     MatButtonModule,
-    MatCardModule
+    MatCardModule,
+    MatSelectModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/find-pet/find-pet.component.ts
+++ src/app/find-pet/find-pet.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
+interface SelectableItem {
+  value: number;
+  viewValue: string;
+}
+
 @Component({
   selector: 'app-find-pet',
   templateUrl: './find-pet.component.html',
@@ -7,9 +12,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FindPetComponent implements OnInit {
 
+  public colors: SelectableItem[] = [
+    { value: 0, viewValue: "Черный" },
+    { value: 1, viewValue: "Белый" },
+    { value: 2, viewValue: "Серый" },
+    { value: 3, viewValue: "Любой" }
+  ];
+
+  public sexes: SelectableItem[] = [{
+    value: 0, viewValue: "м"
+  }, { value: 1, viewValue: "ж" },
+  { value: 2, viewValue: "Любой" }
+  ]
+
+  public maleAnimals: SelectableItem[] = [{ value: 0, viewValue: "Кот" }, { value: 1, viewValue: "Пёс" }];
+  public femaleAnimals: SelectableItem[] = [{ value: 0, viewValue: "Кошка" }, { value: 1, viewValue: "Собака" }];
+  public animalsList: SelectableItem[] = this.maleAnimals;
+
+  public advertTypes: SelectableItem[] = [{ value: 0, viewValue: "Пропал" }, { value: 1, viewValue: "Найден" }]
+
+  public selectedOptions = { color: 3, sex: 2, animal: 0, advertType: 0 };
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  public sexChange(sex: number) {
+    this.animalsList = sex === 1 ? this.femaleAnimals : this.maleAnimals;
   }
 
 }
